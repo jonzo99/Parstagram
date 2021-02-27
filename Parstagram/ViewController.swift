8e4b5329f5746b8d20be012a873ab45b486d1562
jonzo99 <jonzojimenez@gmail.com>
2021-02-26 20:17:24 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by solangel gamez on 2/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
